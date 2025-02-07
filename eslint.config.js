6c18e4f7624392747678e07a01cedf0fd5185a4c
Vladislav Titov <42722011+vvtitov@users.noreply.github.com>
2025-02-07 12:24:07 -0300

    add: esling config

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,32 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+/** @type {import('eslint').Linter.Config} */
+export default {
+  settings: { react: { version: "19.0" } },
+  plugins: ["react"],
+  overrides: [
+    {
+      files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    },
+    {
+      files: ["**/*.js"],
+      languageOptions: {
+        sourceType: "commonjs",
+      },
+    },
+  ],
+  languageOptions: {
+    globals: globals.browser,
+    parserOptions: {
+      project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  ...pluginJs.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...pluginReact.configs.flat.recommended,
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>LUNA HUAPI</h1>
+    </>
+  );
 }
 
 export default App;
